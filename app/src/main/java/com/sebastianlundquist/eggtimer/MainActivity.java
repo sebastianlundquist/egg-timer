072aa527549acf7fb703f19595b687b0ae4502d4
Sebastian Lundquist <sebastian.lundquist@gmail.com>
2019-10-10 20:55:19 +0200

    Remove blank lines

--- app/src/main/java/com/sebastianlundquist/eggtimer/MainActivity.java
+++ app/src/main/java/com/sebastianlundquist/eggtimer/MainActivity.java
@@ -77,14 +77,10 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
+            public void onStartTrackingTouch(SeekBar seekBar) { }
 
             @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
+            public void onStopTrackingTouch(SeekBar seekBar) { }
         });
     }
 }
